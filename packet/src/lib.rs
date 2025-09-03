b50b3b16a7cf81256cc5dbca9d3976c15ba16301
Conor Deegan <conorjdeegan@gmail.com>
2025-09-03 16:30:12 +0100

    updates PACKET_DATA_SIZE to accomodate ML_DSA

--- packet/src/lib.rs
+++ packet/src/lib.rs
@@ -24,12 +24,14 @@ use {
 };
 
 #[cfg(test)]
-static_assertions::const_assert_eq!(PACKET_DATA_SIZE, 1232);
+static_assertions::const_assert_eq!(PACKET_DATA_SIZE, 5704);
 /// Maximum over-the-wire size of a Transaction
 ///   1280 is IPv6 minimum MTU
 ///   40 bytes is the size of the IPv6 header
 ///   8 bytes is the size of the fragment header
-pub const PACKET_DATA_SIZE: usize = 1280 - 40 - 8;
+// pub const PACKET_DATA_SIZE: usize = 1280 - 40 - 8;
+// updating packet data size to accomodate ML_DSA
+pub const PACKET_DATA_SIZE: usize = 5704;
 
 #[cfg(feature = "bincode")]
 pub trait Encode {
